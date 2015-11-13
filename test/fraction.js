8a98c33e892a784c75bf1f2f67bc5d82489393d2
Damian Krzeminski <pirxpilot@code42day.com>
2015-11-13 15:34:34 -0500

    extend the range of characters used to encode fraction
    all values between 0 and 0xFFFF can be used since one can easily
    translate between char and integer representation of them

--- test/fraction.js
+++ test/fraction.js
@@ -1,6 +1,14 @@
 // var should = require('should');
 var fraction = require('../lib/fraction');
 
+
+describe('effective char range', function() {
+  it('should be able to store char', function() {
+    String.fromCharCode(0).charCodeAt(0).should.eql(0);
+    String.fromCharCode(0xFFFF).charCodeAt(0).should.eql(0xFFFF);
+  });
+});
+
 describe('fraction', function () {
 
 
@@ -8,14 +16,14 @@ describe('fraction', function () {
 
     it('find a fraction between two values', function () {
       fraction.between('0', 'z').should.be.eql('U');
-      fraction.between('e', 'f').should.be.eql('eU');
-      fraction.between('0', '1').should.be.eql('0U');
+      fraction.between('e', 'f').should.be.eql('e翿');
+      fraction.between('0', '1').should.be.eql('0翿');
 
       fraction.between('U', 'z').should.be.eql('g');
-      fraction.between('yU', 'z').should.be.eql('yg');
+      fraction.between('yU', 'z').should.be.eql('y耪');
 
 
-      fraction.between('abb', 'abbdefZ').should.be.eql('abbJ');
+      fraction.between('abb', 'abbdefZ').should.be.eql('abb2');
 
       fraction.between('aBBcccc', 'aZ').should.be.eql('aN');
     });
@@ -25,7 +33,6 @@ describe('fraction', function () {
 
       for (i = 0; i < 1000; i++) {
         b = fraction.between(from, to);
-        // console.log(from, '\t', b, '\t', to);
         fraction.compare(from, b).should.eql(-1);
         fraction.compare(to, b).should.eql(1);
         to = b;
@@ -57,7 +64,7 @@ describe('fraction', function () {
         }
       }
 
-      // console.log(b.length);
+      console.log(b.length);
       b.length.should.be.below(1000);
     });
 
@@ -68,6 +75,9 @@ describe('fraction', function () {
     it('should return 0 for equal', function () {
       fraction.compare('abc', 'abc').should.eql(0);
       fraction.compare('0Ac', '0Ac').should.eql(0);
+
+
+      fraction.compare(String.fromCharCode([0, 0xFFFF, 0xEA10]), String.fromCharCode([0, 0xFFFF, 0xEA10]));
     });
 
     it('should detect -1 for <', function () {
