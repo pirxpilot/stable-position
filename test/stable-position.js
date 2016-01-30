6a058ac8b7ee20f2aa7bccbb8357141ef938ea16
Damian Krzeminski <pirxpilot@code42day.com>
2016-01-29 19:49:32 -0500

    remove usused legacy stable-position module

--- test/stable-position.js
+++
@@ -1,7 +0,0 @@
-// var should = require('should');
-// var stablePosition = require('../');
-
-describe('stable-position node module', function () {
-  it('must have at least one test', function () {
-  });
-});
