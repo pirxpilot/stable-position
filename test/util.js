abeef58b3c9171b6afeb2dbbe45895d96cdd8404
Damian Krzeminski <pirxpilot@furkot.com>
2019-04-11 12:09:56 -0600

    use tape instead of mocha/should

--- test/util.js
+++ test/util.js
@@ -1,66 +1,77 @@
-var util = require('../lib/util');
+const test = require('tape');
+const { charAt, split, padRight, convert } = require('../lib/util');
 
-describe('charAt', function () {
-  it('should work for all lenghts', function () {
-    util.charAt('abc', 0, 'x').should.eql('a');
-    util.charAt('abc', 1, 'x').should.eql('b');
-    util.charAt('abc', 2, 'x').should.eql('c');
-    util.charAt('abc', 3, 'x').should.eql('x');
-    util.charAt('abc', 100, 'x').should.eql('x');
+test('charAt', function (t) {
+  t.test('should work for all lenghts', function (t) {
+    t.plan(5);
+
+    t.equal(charAt('abc', 0, 'x'), 'a');
+    t.equal(charAt('abc', 1, 'x'), 'b');
+    t.equal(charAt('abc', 2, 'x'), 'c');
+    t.equal(charAt('abc', 3, 'x'), 'x');
+    t.equal(charAt('abc', 100, 'x'), 'x');
   });
 });
 
 
-describe('split', function () {
-  it('should work for even', function () {
-    util.split(4).should.eql([2, 2]);
-    util.split(100).should.eql([50, 50]);
+test('split', function (t) {
+  t.test('should work for even', function (t) {
+    t.plan(2);
+    t.same(split(4), [2, 2]);
+    t.same(split(100), [50, 50]);
   });
 
-  it('should work for odd', function () {
-    util.split(5).should.eql([2, 3]);
-    util.split(101).should.eql([50, 51]);
-  });
-});
-
-describe('padRight', function() {
-  it('should slice longer string', function () {
-    util.padRight('abcdefgh', 3, 'z').should.eql('abc');
-    util.padRight('ab', 1, 'z').should.eql('a');
-  });
-
-  it('should pad shorter strings', function() {
-    util.padRight('abc', 5, 'z').should.eql('abczz');
-    util.padRight('ab', 3, 'z').should.eql('abz');
-
-  });
-
-  it('should keep unchaged strings with adequate length', function() {
-    util.padRight('abcd', 4, 'z').should.eql('abcd');
-    util.padRight('ab', 2, 'z').should.eql('ab');
+  t.test('should work for odd', function (t) {
+    t.plan(2);
+    t.same(split(5), [2, 3]);
+    t.same(split(101), [50, 51]);
   });
 });
 
-describe('convert', function() {
-  it('should create fractions for integer values', function() {
-    util.convert(0).should.be.eql('\u7000');
-    util.convert(5).should.be.eql('\u7005');
-    util.convert(-10).should.be.eql('\u6FF6');
+test('padRight', function (t) {
+  t.test('should slice longer string', function (t) {
+    t.plan(2);
+    t.equal(padRight('abcdefgh', 3, 'z'), 'abc');
+    t.equal(padRight('ab', 1, 'z'), 'a');
   });
 
-  it('should create fractions for double values', function() {
-
-    util.convert(0.46).should.be.eql('\u7000\u75c2\u8f5c\u28f5\uc400');
-    util.convert(5.244).should.be.eql('\u7005\u3e76\uc8b4\u3958');
-
-    util.convert(-10.98423234).should.be.eql('\u6FF6\u0409\u5970\u08D1\u7fff');
-    util.convert(-10.98423233).should.be.eql('\u6FF6\u0409\u599A\uFBEF\u7fff');
+  t.test('should pad shorter strings', function (t) {
+    t.plan(2);
+    t.equal(padRight('abc', 5, 'z'), 'abczz');
+    t.equal(padRight('ab', 3, 'z'), 'abz');
   });
 
-  it('should maintain monotonicity', function() {
-    (util.convert(0.46) < util.convert(5.244)).should.be.ok();
-    (util.convert(-5.244) < util.convert(0.46)).should.be.ok();
-    (util.convert(-10.5) < util.convert(-10.4)).should.be.ok();
-    (util.convert(-11.3) < util.convert(-10.4)).should.be.ok();
+  t.test('should keep unchaged strings with adequate length', function (t) {
+    t.plan(2);
+    t.equal(padRight('abcd', 4, 'z'), 'abcd');
+    t.equal(padRight('ab', 2, 'z'), 'ab');
+  });
+});
+
+test('convert', function (t) {
+  t.test('should create fractions for integer values', function (t) {
+    t.plan(3);
+    t.equal(convert(0), '\u7000');
+    t.equal(convert(5), '\u7005');
+    t.equal(convert(-10), '\u6FF6');
+  });
+
+  t.test('should create fractions for double values', function (t) {
+    t.plan(4);
+
+    t.equal(convert(0.46), '\u7000\u75c2\u8f5c\u28f5\uc400');
+    t.equal(convert(5.244), '\u7005\u3e76\uc8b4\u3958');
+
+    t.equal(convert(-10.98423234), '\u6FF6\u0409\u5970\u08D1\u7fff');
+    t.equal(convert(-10.98423233), '\u6FF6\u0409\u599A\uFBEF\u7fff');
+  });
+
+  t.test('should maintain monotonicity', function (t) {
+    t.plan(4);
+
+    t.ok(convert(0.46) < convert(5.244));
+    t.ok(convert(-5.244) < convert(0.46));
+    t.ok(convert(-10.5) < convert(-10.4));
+    t.ok(convert(-11.3) < convert(-10.4));
   });
 });
